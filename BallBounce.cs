ab73c4d41d2866a1c88ab912338bdb2e166d8717
bandicootz <78934705+bandicootz@users.noreply.github.com>
2021-04-09 12:16:31 +0200

    Add files via upload
    Пока это не что-то из разряда "вау", но я с каждым днем узнаю новое и учусь это использовать.

---
+++ BallBounce.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    private Rigidbody ballRb;
+    Vector3 lastVelocity;
+    public Vector3 startPos;
+    private GameManager gameManager;
+
+    bool bounce = false;
+
+    void Awake()
+    {
+        ballRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        StartCoroutine(waitForDestroy());
+
+        Debug.Log(gameManager.speedThrow);
+        ballRb.AddRelativeForce(Vector3.back * gameManager.speedThrow);
+    }
+
+    IEnumerator waitForDestroy()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+        gameManager.canThrowBall = true;
+    }
+
+    void FixedUpdate()
+    {
+        lastVelocity = ballRb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Border"))
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, col.contacts[0].normal);
+            ballRb.velocity = direction * speed;
+            if (!bounce)
+            {
+                bounce = true;
+            }
+
+        }
+
+        if (col.gameObject.CompareTag("EnemyBlock"))
+        {
+            col.gameObject.SetActive(false);
+            gameManager.blocksDestroyed++;
+            Destroy(gameObject);
+            gameManager.canThrowBall = true;
+        }
+
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            gameManager.canThrowBall = true;
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    private int score;
+    public int pointValue;
+    public bool isGameActive;
+
+    private float spawnRate = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
